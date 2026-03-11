c011864f6aa63b3744230e5c6c165a8287426893
garaged <garaged@gmail.com>
2026-03-10 21:09:32 -0600

    Fix ci/cd (#1)
    * Revert "Add explicit watch app icon variants for CI"
    
    This reverts commit 77165103034cf304cf10237ff0f6f390f24d7cda.
    
    * Revert "Fixing AppIcon for watch app"
    
    This reverts commit aff2ad9f07f85b7e0917082821aaaaccebe66a49.
    
    * Fixing tests
    
    * More CI/CD build fixes

---
+++ watchkitapp Watch App/watchkitappApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchkitappApp.swift
+//  watchkitapp Watch App
+//
+//  Created by Max Valdez on 08/03/26.
+//
+
+import SwiftUI
+
+@main
+struct watchkitapp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
